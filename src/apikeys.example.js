f7f1e5cf1659c370394cbe05ee446f571a4a1f00
meghein <meghan.hein@gmail.com>
2020-11-27 23:41:03 -0800

    add mock data

---
+++ src/apikeys.example.js
@@ -0,0 +1,5 @@
+// replace IDs with emailJS values and change file name it 'apikeys.js':
+export default {
+    USER_ID :'user_id_number_goes_here',
+    TEMPLATE_ID:'template_id_number_goes_here'
+}
