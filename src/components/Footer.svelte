42f066a8fdc7f7d094b4184ec30a6d192d5836e3
meghein <meghan.hein@gmail.com>
2020-11-27 17:08:21 -0800

    add flex

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -18,9 +18,11 @@ footer {
 <footer>
   <p className='name'>{@html '© 2020 Meghan Hein</br>All rights reserved'}</p>
   <div className='links'>
-    Let's connect!
-    <a href='https://www.instagram.com/meghein/' target='_blank' rel='noreferrer'><Icon data={instagram} scale='2'/></a>
-    <a href='https://www.linkedin.com/in/meghan-hein/' target='_blank' rel='noreferrer'><Icon data={linkedinSquare} scale='2'/></a>
-    <a href='https://github.com/meghein' target='_blank' rel='noreferrer'><Icon data={githubAlt} scale='2'/></a>
+    <p>Let's connect!</p>
+    <div>
+      <a href='https://www.instagram.com/meghein/' target='_blank' rel='noreferrer'><Icon data={instagram} scale='2'/></a>
+      <a href='https://www.linkedin.com/in/meghan-hein/' target='_blank' rel='noreferrer'><Icon data={linkedinSquare} scale='2'/></a>
+      <a href='https://github.com/meghein' target='_blank' rel='noreferrer'><Icon data={githubAlt} scale='2'/></a>
+    </div>
   </div>
 </footer>
