d56e2593bca4cf74113aa5a5d0f15d66445a84a3
meghein <meghan.hein@gmail.com>
2020-11-28 16:08:32 -0800

    clean up welcome message and style button

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -24,12 +24,34 @@
 <style type="text/scss">
 	#home {
     min-height: 100vh;
-    background: linear-gradient(165deg, #FBEEC1 60%,#da862d calc(60% + 2px));
+    background: linear-gradient(165deg, #FBEEC1 60%,#DA862D calc(60% + 2px));
+    display: grid;
+    place-items: center;
     h1 {
+      font-family: 'Libre Baskerville', serif;
       margin-bottom: 0;
+      padding-left: .5em;
+      padding-right: .5em;
       font-size: 4em;
       font-weight: 100;
-      // font-family: 'Syne', sans-serif; 
+      text-decoration: underline solid #DA862D;
+      text-decoration-thickness: 2px;
+      border-bottom: 2px solid #DA862D;
+    }
+    button {
+      margin-top: 1.5em;
+      width: 70px;
+      height: 60px;
+      animation: pulse 2.5s infinite;
+      animation-timing-function: linear;   
+      &:hover{ 
+        animation: none;
+      }
+      @keyframes pulse {
+        0% { transform: scale(1); }
+        50% { transform: scale(1.1); }
+        100% { transform: scale(1); }
+      }
     }
 	}
 </style>
@@ -38,10 +60,12 @@
 
 <main>
   <div id='home' use:watchResize={handleResize}>
-    <h1>Hi, I'm Meghan.</h1>
-    <h2>Thanks for stopping by!</h2>
-    <h5>I am passionate abot building excellent software that improves the lives of those around me.</h5>
-    <button on:click|preventDefault={() => scroll()}>click here</button>
+    <div>
+      <h1>I'm Meghan.</h1>
+      <h2>a full stack web developer.</h2>
+      <!-- <h5>I am passionate abot building excellent software that improves the lives of those around me.</h5> -->
+      <button on:click|preventDefault={() => scroll()}>Learn more </button>
+    </div>
   </div>
   <Nav navY={windowHeight}/>
   <About/>
