5ed24ae674da8b8448aa871c046a4c5f633080e9
meghein <meghan.hein@gmail.com>
2020-11-30 12:06:58 -0800

    fix colour scheme

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -3,14 +3,14 @@
   import { afterUpdate } from 'svelte';
   import apiKeys from '../apikeys';
 
-  let confirmation
+  let confirmation;
 
   function sendEmail(e) {
     const contact = {
       name: e.target.name.value,
       email: e.target.email.value,
       message: e.target.message.value,
-    }
+    };
 
     confirmation = 
         `<h2>Thanks for reaching out ${contact.name}!</h2>
@@ -30,60 +30,79 @@
   }
 
   function confirmationMsg() {
-    const message = document.getElementById('confirm-msg')
+    const message = document.getElementById('confirm-msg');
     if (confirmation) {
-      message.setAttribute('style', 'visibility: visible')
-      document.getElementById('contact-form').reset()
-
+      message.setAttribute('style', 'visibility: visible');
+      document.getElementById('contact-form').reset();
       setTimeout(() => {
-        message.setAttribute('style', 'visibility: hidden')
-        confirmation = undefined
-      }, 5000)
+        message.setAttribute('style', 'visibility: hidden');
+        confirmation = undefined;
+      }, 5000);
 
     } else {
-      message.setAttribute('style', 'visibility: hidden')
+      message.setAttribute('style', 'visibility: hidden');
     }
-  }
+  };
 
   afterUpdate(() => {
-    confirmationMsg()
-  })
+    confirmationMsg();
+  });
 
 </script>
 
 <style type="text/scss">
   #contact {
-    min-height: 75vh;
+    min-height: 80vh;
     background: linear-gradient(176deg, #006f69 70%, #FBEEC1 calc(70% + 2px));
     h1 {
       margin-top: 0;
       color:#6a040f;
+      font-family: 'Libre Baskerville', serif;
       i {
-        color:#b3a577;
+        color:#FBEEC1;
       }
     }
     form {
       display: grid;
       margin: auto;
       width: 60%;
-      input, textarea, button {
-        background-color: inherit;
-        // border: 1px dotted #da862d; 
-        border: 1px dotted #5c5c5c;
+      input, textarea {
+        border: none;
+        color: #6a040f;
+        background-color: #FBEEC1;
+        &::placeholder {
+          color: #011f51;
+        }
+        &:focus, &:active, &:hover {
+          outline: none;
+        }
+        &:-webkit-autofill,
+        &:-webkit-autofill:hover,
+        &:-webkit-autofill:focus,
+        &:-webkit-autofill:active  {
+          box-shadow: 0 0 0 30px  #FBEEC1 inset;
+          -webkit-box-shadow: 0 0 0 30px  #FBEEC1 inset !important;
+          -webkit-text-fill-color: #6a040f !important;
+        }
       }
       textarea {
         min-height: 8em;
       }
       button {
         margin: auto;
-        background-color: #5c5c5c;
-        color: #181818;
         font-size: 1.2em;
+        color: #FBEEC1;
+        border: 1px solid #FBEEC1;
+        &:hover {
+          color: #167974;
+          background-color: #FBEEC1;
+        }
       }
     }
     #confirm-msg {
       margin: auto;
       width: 60%;
+      color: #FBEEC1;
     }
   }  
 </style>
