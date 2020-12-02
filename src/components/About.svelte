17e0d8293cb873cc1bb5a600cb2c3fcb3be6c559
meghein <meghan.hein@gmail.com>
2020-12-01 21:59:03 -0800

    add transition for title

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -3,20 +3,18 @@
   import { fade, fly } from 'svelte/transition';
   import { y, windowHeight } from './stores.js';
 
-  let visible = true;
+  let visible = false;
 
   afterUpdate(() => {
-    const header = document.getElementById('about-head');
-    const topHead = header.getBoundingClientRect().top;
-    // if (topHead / $y > 2.5) {
-    //   visible = !visible
-    // }
-    console.log(topHead, $y)
+    const header = document.getElementById('scroll-about');
+    let topHead = header.getBoundingClientRect().top;
+    $y
+    if (topHead < $windowHeight && topHead > $windowHeight*-.75) {
+      visible = true
+    } else {
+      visible = false
+    }
   })
-
-  // onMount(() => {
-  //   visible = !visible
-  // })
   
 </script>
 
@@ -33,9 +31,9 @@
 </style>
 
 <div id='about'>
-  <div class='top'/>
+  <div id='scroll-about' class='top'/>
   {#if visible}
-  <h1 id='about-head' in:fly="{{ y: 200, duration: 2000 }}" out:fade>
+  <h1 id='about-head' in:fly="{{ x: 1000, duration: 2000 }}" out:fade>
     ABOUT
   </h1>
   {/if}
