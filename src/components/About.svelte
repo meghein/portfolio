fc2736d58c4b3576a693af0c7a4bc8b1742713b8
meghein <meghan.hein@gmail.com>
2020-12-03 23:01:46 -0800

    add profile photo

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -31,27 +31,24 @@
     ABOUT
   </h1>
   {/if}
-  <img src="https://i.pinimg.com/originals/b7/77/60/b7776009c68eeac18729480ac34f51a6.gif" alt="big mistake">
-  <div id='bio'>
-    <p>Hi there! Thanks for visiting my portfolio!</p>
-    
-    <p>I'm a web developer with a diverse background ranging from direct action activism in the high seas to forging silver in the Bajio mountains of central Mexico. I recently graduated from <a href='https://www.lighthouselabs.ca/' target='_blank' rel='noreferrer'>Lighthouse Labs'</a> immersive web development boot camp, which reinforced my infatuation with the programmatic potential in shaping the future.</p>
+  <div id='profile'>
+    <img src="images/profile.jpeg" alt="my face">
+    <div id='bio'>
+      <p>Hi there! Thanks for visiting my portfolio!</p>
       
-    <p>Living on the westcoast of Canada, dwelling at the intersection of creativity and logic, and inspired equally by complex code and sleek design - I write code and fervently build upon my development dexterity every single day.</p>
-    <p>Want to know more? <a href='https://www.coursereport.com/blog/how-meghan-became-a-digital-nomad-with-lighthouse-labs' target='_blank' rel='noreferrer'>Click here to read an interview </a>I did with Course Report after graduating from Lighthouse Labs.</p>
-
-    <button on:click={toggleModal}>View my resume here</button>    
+      <p>I'm a web developer with a diverse background ranging from direct action activism in the high seas to forging silver in the Bajio mountains of central Mexico. I recently graduated from <a href='https://www.lighthouselabs.ca/' target='_blank' rel='noreferrer'>Lighthouse Labs'</a> immersive web development boot camp, which reinforced my infatuation with the programmatic potential in shaping the future.</p>
+        
+      <p>Living on the westcoast of Canada, dwelling at the intersection of creativity and logic, and inspired equally by complex code and sleek design - I write code and fervently build upon my development dexterity every single day.</p>
+      <p>Want to know more? <a href='https://www.coursereport.com/blog/how-meghan-became-a-digital-nomad-with-lighthouse-labs' target='_blank' rel='noreferrer'>Click here to read an interview </a>I did with Course Report after graduating from Lighthouse Labs.</p>
+  
+      <button on:click={toggleModal}>View my resume here</button>    
+    </div>
   </div>
   {#if resume}
-  <div class='modal'>
-    <div
-      id='resume'
-      use:clickOutside on:click_outside={toggleModal}
-      in:fly="{{ y: 1000, duration: 1000 }}"
-      out:fly="{{ y: -1000, duration: 1000 }}"
-    >
+  <div class='modal' out:fly="{{ y: -1000, duration: 1000 }}">
+    <div id='resume' in:fly="{{ y: 1000, duration: 1000 }}" use:clickOutside on:click_outside={toggleModal}>
       <i id='close' on:click={toggleModal} class="far fa-times-circle"></i>
-      <iframe title='Meghan Hein 2020' src="images/resume.pdf" width='850px' height={window.innerHeight - 60}>
+      <iframe title='Meghan Hein 2020' src="images/resume.pdf" width={window.innerWidth - 200} height={window.innerHeight - 60}>
       </iframe>
     </div>
   </div>
