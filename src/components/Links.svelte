694a89d50a5635e58b312849995659c83b1fb846
meghein <meghan.hein@gmail.com>
2020-12-04 01:13:07 -0800

    fix hover animation

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -15,30 +15,41 @@
 	#link-header {
 		margin: 0;
 		padding-bottom: 2px;
-	}	
-	.fab {
-		font-size: 2em;
-		&:hover{
-			animation: flip 2s infinite;
+	}
+	.icons {
+		&:hover > a:not(:hover) {
+			opacity: 0.5;
 		}
-		@keyframes flip {
-			0% { transform: scaleX(1); }
-			50% { transform: scaleX(-1); }
-			100% { transform: scaleX(1); }
-		}
+		.fab {
+			font-size: 2em;
+			padding: 2px;
+			&:hover{
+				animation: shake .5s 1 linear;
+			}
+			@keyframes shake{
+				0%{ transform:rotate(10deg) scale(1.1) }
+				20%{ transform:rotate(-10deg) scale(1.1) }
+				40%{ transform:rotate(10deg) scale(1.1) }
+				60%{ transform:rotate(-10deg) scale(1.1) }
+				80%{ transform:rotate(10deg) scale(1.1) }
+				100%{ transform:rotate(0) scale(1.1) }
+			}
+	}
 	}
 </style>
 
 <div class='links' style='font-size: {size}'>
 	<p id='link-header'>{msg}</p>
-	<div>
+	<div class='icons'>
 		{#each icons as {icon, link} }
-		<a href='{link}' target='_blank' rel='noreferrer'>
-				<i
-					class="fab {icon}"
-					aria-hidden="true"
-					style='color: {color}'
-				/>
+		<a
+			href='{link}'
+			target='_blank'
+			rel='noreferrer'
+			class="fab {icon}"
+			aria-hidden="true"
+			style='color: {color}'
+			>
 			</a>
     {/each}
 	</div>
