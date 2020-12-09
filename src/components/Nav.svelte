0f5059972807bdaaf98396a557722391113d607b
meghein <meghan.hein@gmail.com>
2020-12-08 23:23:58 -0800

    fix nav font-size

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,9 +1,8 @@
 <script>
-  import { y, windowHeight, windowWidth } from '../store.js';
+  import { y, windowHeight } from '../store.js';
   import { afterUpdate } from "svelte";
-  import { clickOutside } from '../helpers/clickOutside'
+  import scroll from '../helpers/scroll'
 
-  let fullMenu, responsiveMenu;
   const list = ['home', 'about', 'projects', 'contact'];
 
   function handleHighlight() {
@@ -20,45 +19,23 @@
     }
   }
 
-  function handleBackground(topMenu) {
-    let fixedMenu;
-
-    if (document.getElementById('nav-items')) {
-      fixedMenu = document.getElementById('nav-items')
-    }
-
-    if (document.getElementById('hamburger')) {
-      fixedMenu = document.getElementById('hamburger')
-    }
-      if (topMenu <= $windowHeight && topMenu > -$windowHeight) {
-        fixedMenu.setAttribute('style', 'background-color: #DA862D;')
-      } else if (topMenu <= -$windowHeight && topMenu > Math.round(-$windowHeight*1.75)) {
-        fixedMenu.setAttribute('style', 'background-color: #FBEEC1;')
-      } else if (topMenu <= Math.round(-$windowHeight*1.8) ) {
-        fixedMenu.setAttribute('style', 'background-color: #006F69;')
-      }
-  }
-
-  function scroll(page) {
-    document
-      .getElementById(page.item)
-      .scrollIntoView({ behavior: 'smooth', block: 'start'});
-    if (responsiveMenu) {
-      responsiveMenu = false
-    }
-    
-  };
-
-  function toggleMenu() {
-    responsiveMenu = !responsiveMenu
-  }
-  
-  afterUpdate(() => {
+  function handleBackground() {
     const menu = document.getElementById('nav-container');
     const topMenu = Math.round(menu.getBoundingClientRect().top);
+    const fixedMenu = document.getElementById('nav-items')
+    console.log(topMenu)
+    if (topMenu <= $windowHeight && topMenu > -$windowHeight) {
+      fixedMenu.setAttribute('style', 'background-color: #DA862D;')
+    } else if (topMenu <= -$windowHeight && topMenu > Math.round(-$windowHeight*1.75)) {
+      fixedMenu.setAttribute('style', 'background-color: #FBEEC1;')
+    // } else if (topMenu <= Math.round(-$windowHeight*1.8) ) {
+    //   fixedMenu.setAttribute('style', 'background-color: #006F69;')
+    }
+  }
+
+  afterUpdate(() => {
     handleHighlight();
-    handleBackground(topMenu);
-    // $windowWidth <= 640 ? fullMenu = false : fullMenu = true
+    handleBackground();
   });
 
 </script>
@@ -69,7 +46,6 @@
 
 <nav id="nav-container">
   <div id='main-nav'>
-    <!-- {#if fullMenu} -->
     <ul id="nav-items" class="{ $y >= $windowHeight ? 'fixed' : '' }">
       {#each list as item }
         <li>
@@ -79,28 +55,6 @@
         </li>
       {/each}
     </ul>
-    <!-- {:else}
-    <div id='hamburger' class='{ $y >= $windowHeight ? 'fixed' : '' }'>
-      <i
-        class="fas fa-bars"
-        on:click={toggleMenu}
-      />
-    </div>
-      {#if responsiveMenu}
-      <ul id="responsive-items">
-        {#each list as item }
-          <li>
-            <a
-              href="#{item}" class="item"
-              on:click|preventDefault={() => (scroll({item}))}
-              use:clickOutside on:click_outside={toggleMenu}>
-              {item.toUpperCase()}
-            </a>
-          </li>
-        {/each}
-      </ul>
-      {/if}
-    {/if} -->
   </div>
 </nav>
 
