fcc4b354de1e7d8e5be4b5b357fd952c039ff808
meghein <meghan.hein@gmail.com>
2020-12-29 21:36:46 -0800

    adjust image size

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -6,7 +6,7 @@
   import { cubicOut, elasticOut } from 'svelte/easing';
 
   let visible = false;
-  let resume;
+  let resume = false;
 
   afterUpdate(() => {
     const header = document.getElementById('scroll-about');
@@ -42,7 +42,7 @@
     in:fly="{{x:10, duration: 1000, easing: cubicOut}}"
     out:fade
     >
-      <img src="images/cutout.png" alt="my face" in:fly="{{x:-10, duration: 500, delay: 1200, easing: cubicOut}}" out:fly="{{x: -10, duration: 500}}"> 
+      <img src="images/cutout.png" alt="my face" in:fly="{{x:-50, duration: 500, delay: 1200}}" out:fly="{{x: -10, duration: 500}}"> 
       <p>
         
         I'm a web developer with a diverse background ranging from direct action activism in the high seas to forging silver in the Bajio mountains of central Mexico. I recently graduated from <a href='https://www.lighthouselabs.ca/' target='_blank' rel='noreferrer'>Lighthouse Labs'</a> immersive web development boot camp, which reinforced my infatuation with the programmatic potential in shaping the future.<br/><br/>
@@ -51,12 +51,12 @@
         
         Want to know more? <a href='https://www.coursereport.com/blog/how-meghan-became-a-digital-nomad-with-lighthouse-labs' target='_blank' rel='noreferrer'>Click here to read an interview </a>I did with Course Report.
       </p>
-      {#if $windowWidth < 769}
+      {#if $windowWidth > 769}
+      <button on:click={toggleModal}>View Resume</button>
+      {:else}
       <button type="submit" on:click={openPdf}>
         View Resume <i class="fas fa-external-link-alt"/>
       </button>
-      {:else}
-      <button on:click={toggleModal}>View Resume</button>
       {/if}
     </div>
   </div>
